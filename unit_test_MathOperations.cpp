87a20c2b9769a78c5bb2bee76800a9608f49ea55
Cornelius-Leary-III <celeary3@vt.edu>
2019-04-15 21:38:46 -0400

    Finished add-single-operation tests.

--- unit_test_MathOperations.cpp
+++ unit_test_MathOperations.cpp
@@ -22,5 +22,71 @@ TEST(test_MathOperations, request_single_operation_Add)
     EXPECT_EQ(listOfRequestedOperations.size(), 1);
     EXPECT_EQ(listOfRequestedOperations.front()->getResult(), 7.8);
     EXPECT_NO_THROW(listOfRequestedOperations.front()->getResult());
+    EXPECT_EQ(listOfRequestedOperations.front()->getType(), operationType::ADD);
+}
+
+TEST(test_MathOperations, request_single_operation_Subtract)
+{
+    MathOperations testMathOperations;
+    
+    testMathOperations.requestOperation(4.5, 1.9, operationType::SUBTRACT);
+    
+    auto listOfRequestedOperations = testMathOperations.getSetOfRequestedOperations();
+    EXPECT_EQ(listOfRequestedOperations.size(), 1);
+    EXPECT_EQ(listOfRequestedOperations.front()->getResult(), 2.6);
+    EXPECT_NO_THROW(listOfRequestedOperations.front()->getResult());
+    EXPECT_EQ(listOfRequestedOperations.front()->getType(), operationType::SUBTRACT);
+}
+
+TEST(test_MathOperations, request_single_operation_Multiply)
+{
+    MathOperations testMathOperations;
+    
+    testMathOperations.requestOperation(4, 12, operationType::MULTIPLY);
+    
+    auto listOfRequestedOperations = testMathOperations.getSetOfRequestedOperations();
+    EXPECT_EQ(listOfRequestedOperations.size(), 1);
+    EXPECT_EQ(listOfRequestedOperations.front()->getResult(), 48);
+    EXPECT_NO_THROW(listOfRequestedOperations.front()->getResult());
+    EXPECT_EQ(listOfRequestedOperations.front()->getType(), operationType::MULTIPLY);
+}
+
+TEST(test_MathOperations, request_single_operation_Divide)
+{
+    MathOperations testMathOperations;
+    
+    testMathOperations.requestOperation(4.5, 2, operationType::DIVIDE);
+    
+    auto listOfRequestedOperations = testMathOperations.getSetOfRequestedOperations();
+    EXPECT_EQ(listOfRequestedOperations.size(), 1);
+    EXPECT_EQ(listOfRequestedOperations.front()->getResult(), 2.25);
+    EXPECT_NO_THROW(listOfRequestedOperations.front()->getResult());
+    EXPECT_EQ(listOfRequestedOperations.front()->getType(), operationType::DIVIDE);
+}
+
+TEST(test_MathOperations, request_single_operation_Remainder)
+{
+    MathOperations testMathOperations;
+    
+    testMathOperations.requestOperation(8, 5, operationType::REMAINDER);
+    
+    auto listOfRequestedOperations = testMathOperations.getSetOfRequestedOperations();
+    EXPECT_EQ(listOfRequestedOperations.size(), 1);
+    EXPECT_EQ(listOfRequestedOperations.front()->getResult(), 3);
+    EXPECT_NO_THROW(listOfRequestedOperations.front()->getResult());
+    EXPECT_EQ(listOfRequestedOperations.front()->getType(), operationType::REMAINDER);
+}
+
+TEST(test_MathOperations, request_single_operation_Exponentiate)
+{
+    MathOperations testMathOperations;
+    
+    testMathOperations.requestOperation(4, 2, operationType::POWER);
+    
+    auto listOfRequestedOperations = testMathOperations.getSetOfRequestedOperations();
+    EXPECT_EQ(listOfRequestedOperations.size(), 1);
+    EXPECT_EQ(listOfRequestedOperations.front()->getResult(), 16);
+    EXPECT_NO_THROW(listOfRequestedOperations.front()->getResult());
+    EXPECT_EQ(listOfRequestedOperations.front()->getType(), operationType::POWER);
 }
 
